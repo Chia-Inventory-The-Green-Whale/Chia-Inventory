f8928cd2c5a9c2dc0ae10b6565677b2bdac59c41
The Green Whale <110241587+Chia-Inventory-The-Green-Whale@users.noreply.github.com>
2022-07-30 20:02:11 +0700

    Update locations.py

--- locations.py
+++ locations.py
@@ -10,22 +10,79 @@ class room:
         self.west = self.location
         self.south = self.location
         self.north = self.location
+        self.area_type = "Wild"
 
 
 def locate(location):
     here = room(location)
     if here.location == 'Kingdom Street':
-        here.description = "Location: Kingdom Street; This place is bustling with people. \nYou noticed that there is an obvious exit: East (Slime Forest)"
-        here.monsters = ['Cockroach', 'Cockroach', 'Cockroach', 'Cockroach', 'Cockroach',
-                         'Cockroach', 'Cockroach', 'Mouse', 'Mouse', 'Mouse']
+        here.description = "Location: Kingdom Street; This place is bustling with people. \nYou noticed following exits: east (East Kingdom Street), north (Tavern), south (Marketplace)"
+        here.monsters = ['Cockroach']*5 + ['Mouse'*5]
+        here.east = 'East Kingdom Street'
+        here.north = 'Tavern'
+        here.south = 'Marketplace'
+        here.area_type = "City"
+
+    if here.location == 'Tavern':
+        here.description = "Location: Tavern. \nYou noticed following exits: south (Kingdom Street)"
+        here.monsters = ['Cockroach']*7 + ['Mouse']*2 + ['Drunk Man']*1
+        here.south = 'Kingdom Street'
+        here.area_type = "City"
+
+    if here.location == 'Marketplace':
+        here.description = "Location: Marketplace; This place is bustling with people. \nYou noticed following exits: north (Kingdom Street)"
+        here.monsters = ['Cockroach']*5 + ['Mouse']*2 + ['Thief']*2 + ['Scum']*1
+        here.north = 'Kingdom Street'
+        here.area_type = "City"
+
+    if here.location == 'East Kingdom Street':
+        here.description = "Location: East Kingdom Street; This place is bustling with people. \nYou noticed following exits: east (Gate of Viridis), west (Kingdom Street), north (Blacksmith Shop), south (Stable)"
+        here.monsters = ['Cockroach']*4 + ['Mouse']*4 + ['Thief']*2
+        here.east = 'Gate of Viridis'
+        here.west = 'Kingdom Street'
+        here.north = 'Blacksmith Shop'
+        here.south = 'Stable'
+        here.area_type = "City"
+
+    if here.location == 'Blacksmith Shop':
+        here.description = "Location: Blacksmith Shop; Adventurers gathered here for equipments. \nYou noticed following exits: south (East Kingdom Street)"
+        here.monsters = ['Mouse']
+        here.south = 'East Kingdom Street'
+        here.area_type = "City"
+
+    if here.location == 'Stable':
+        here.description = "Location: Stable; Adventurers gathered here for mounts, such as deers. \nYou noticed following exits: north (East Kingdom Street)"
+        here.monsters = ['Cockroach']*5 + ['Mouse']*5
+        here.north = 'East Kingdom Street'
+
+    if here.location == 'Gate of Viridis':
+        here.description = "Location: Gate of Viridis. \nYou noticed following exits: west (East Kingdom Street), east (Border of Slime Forest)"
+        here.monsters = ['Cockroach']*5 + ['Mouse']*4 + ['Gate Guard']
+        here.east = 'Border of Slime Forest'
+        here.west = 'East Kingdom Street'
+
+    if here.location == 'Border of Slime Forest':
+        here.description = "Location: Border of Slime Forest; You entered the forest. \nYou noticed following exits: west (Gate of Viridis), east (Slime Forest)"
+        here.monsters = ['Slime']*7 + ['Forest Slime']*3
+        here.west = 'Gate of Viridis'
         here.east = 'Slime Forest'
 
-
     if here.location == 'Slime Forest':
-        here.description = "Location: Slime Forest; You are surrounded by woods. \nYou noticed that there is an obvious exit: West (Kingdom Street)"
-        here.monsters = ['Forest Slime', 'Forest Slime', 'Forest Slime', 'Forest Slime', 'Forest Slime',
-                         'Aquatic Slime', 'Forest Slime', 'Forest Slime', 'Aquatic Slime',
-                         'Swamp Slime']
-        here.west = 'Kingdom Street'
+        here.description = "Location: Slime Forest; You are surrounded by woods. \nYou noticed following exits: west (Border of Slime Forest), east (Deep Slime Forest), north (A Lake in Slime Forest)"
+        here.monsters = ['Slime']*3 + ['Forest Slime']*7
+        here.west = 'Border of Slime Forest'
+        here.east = 'Deep Slime Forest'
+        here.north = 'A Lake in Slime Forest'
+
+    if here.location == 'A Lake in Slime Forest':
+        here.description = "Location: A Lake in Slime Forest; You see a lake here. \nYou noticed following exits: south (Slime Forest)"
+        here.monsters = ['Forest Slime']*5 + ['Aquatic Slime']*5
+        here.south = 'Slime Forest'
+
+    if here.location == 'Deep Slime Forest':
+        here.description = "Location: Deep Slime Forest; You are surrounded by woods. \nYou noticed following exits: west (Slime Forest)"
+        here.monsters = ['Forest Slime']*7 + ['Bat']*1 + ['Hungry Wolf']*1 + ['Swamp Slime']*1
+        here.west = 'Slime Forest'
+
 
     return here
