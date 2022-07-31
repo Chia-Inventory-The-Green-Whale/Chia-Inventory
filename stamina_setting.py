a486bb3c84e7b313704d7613845d20a87be1cdb6
The Green Whale <110241587+Chia-Inventory-The-Green-Whale@users.noreply.github.com>
2022-07-31 10:40:35 +0700

    Create stamina_setting.py

---
+++ stamina_setting.py
@@ -0,0 +1,12 @@
+# all about stamina here
+# cost
+hunt_cost = 200
+go_cost = 20
+
+# max stamina
+city_max_stamina = 1800
+wild_max_stamina = 600
+
+# charge rate
+city_charge = 1.5
+wild_charge = 0.5
