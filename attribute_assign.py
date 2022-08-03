312c5376f7d0f4a861ee45793948ce063cc7edea
The Green Whale <110241587+Chia-Inventory-The-Green-Whale@users.noreply.github.com>
2022-08-03 14:13:40 +0700

    Update attribute_assign.py

--- attribute_assign.py
+++ attribute_assign.py
@@ -4,27 +4,52 @@ from library.item_list import *
 
 
 def assign_attribute(stats):
+    # Chia Inventory
     if stats.weapon in Knife_list:
         stats.slash = 1
         stats.pierce = 1
+        stats.luc += int(stats.level / 5) * random.randint(1, 5)
+        stats.dex += int(stats.level / 5) * random.randint(1, 5)
+
     if stats.weapon in Sword_list:
         stats.slash = 2
         stats.bash = 1
+        stats.str += int(stats.level / 5) * random.randint(1, 5)
+        stats.dex += int(stats.level / 5) * random.randint(1, 5)
+
     if stats.weapon in Short_Axe_list:
         stats.slash = 2
         stats.bash = 1
+        stats.str += int(stats.level / 5) * random.randint(1, 5)
+        stats.dex += int(stats.level / 5) * random.randint(1, 5)
+
     if stats.weapon in Wood_Club_list:
         stats.bash = 2
+        stats.str += int(stats.level / 5) * random.randint(1, 5)
+        stats.con += int(stats.level / 5) * random.randint(1, 5)
+
     if stats.weapon in Short_Bow_list:
         stats.pierce = 2
+        stats.dex += int(stats.level / 4) * random.randint(1, 5)
+
     if stats.weapon in Catapult_list:
         stats.bash = 1
+        stats.str += int(stats.level / 6) * random.randint(1, 5)
+        stats.con += int(stats.level / 6) * random.randint(1, 5)
+
     if stats.weapon in Enhanced_Tree_Root_list:
         stats.pierce = 1
+        stats.luc += int(stats.level / 6) * random.randint(1, 5)
+        stats.dex += int(stats.level / 6) * random.randint(1, 5)
+
     if stats.weapon in Khopesh_list:
         stats.slash = 1
         stats.bash = 1
         stats.pierce = 1
+        stats.str += int(stats.level / 8) * random.randint(1, 5)
+        stats.dex += int(stats.level / 8) * random.randint(1, 5)
+        stats.con += int(stats.level / 8) * random.randint(1, 5)
+
     if stats.weapon in Red_Nuclei_Weapon_list:
         stats.str = stats.str + random.randint(1, 5)
     if stats.weapon in Orange_Nuclei_Weapon_list:
@@ -33,16 +58,20 @@ def assign_attribute(stats):
         stats.dex = stats.dex + random.randint(1, 5)
     if stats.weapon in Purple_Nuclei_Weapon_list:
         stats.wis = stats.wis + random.randint(1, 5)
+
     if stats.weapon in Shadow_Sword:
         stats.slash = 5
         stats.pierce = 1
         stats.magic = 3
+
     if stats.shield in Bark_Shield_list:
-        stats.defense = 1
-    if stats.mount in Deer_list:
-        stats.dex = stats.dex + random.randint(1, 5)
-    if stats.mount in Majestic_Deer_list:
-        stats.str = stats.str + random.randint(1, 5)
+        stats.defense += 1 + int(stats.level / 5)
+
+    if stats.armor in Cloth_Armor_List:
+        stats.defense += 1
+        stats.health += int(stats.level / 5)
+        stats.dex += int(stats.level / 5) * random.randint(1, 5)
+
     if stats.ring == "Nuclei Ring 01":
         stats.str = stats.str + random.randint(1, 5)
     if stats.ring == "Nuclei Ring 02":
@@ -53,10 +82,22 @@ def assign_attribute(stats):
         stats.con = stats.con + random.randint(1, 5)
     if stats.ring == "Nuclei Ring 05":
         stats.wis = stats.wis + random.randint(1, 5)
+
+    # Deer
+    if stats.mount in Deer_list:
+        stats.dex = stats.dex + random.randint(1, 5)
+    if stats.mount in Majestic_Deer_list:
+        stats.str = stats.str + random.randint(1, 5)
+
+    # Brave Seedling
     if stats.herb in Brave_Seedling_list:
         stats.health = stats.health + random.randint(1, 5)
     if stats.herb in Powerful_Brave_Seedling_list:
         stats.health = stats.health + random.randint(1, 5)
+    if stats.herb in Super_Powerful_Brave_Seedling_list:
+        stats.con = stats.con + random.randint(1, 5)
+
+    # Chia Slime
     if stats.familiar in Red_Chia_Slime_list:
         stats.str = stats.str + 1
         stats.int = stats.int - 1
@@ -83,13 +124,15 @@ def assign_attribute(stats):
         Identify_castchance = random.randint(1, 20)
         if Identify_castchance in [17, 18, 19, 20]:
             stats.int = stats.int + random.randint(2, 4)
+
+    # Snails
     if stats.familiar in Snail_List:
         stats.dex = stats.dex - random.randint(1, 5)
         stats.luc = stats.luc + random.randint(1, 5)
-    if stats.herb in Super_Powerful_Brave_Seedling_list:
-        stats.con = stats.con + random.randint(1, 5)
     if stats.familiar in Hard_Snail_list:
         stats.con = stats.con + random.randint(1, 5)
+
+    # Chia Farmers
     if stats.portrait in CeruleanBlueSkin_Chia_Farmers_List:
         stats.int = stats.int + 1
     if stats.portrait in CyanSkin_Chia_Farmers_List:
@@ -123,7 +166,6 @@ def assign_attribute(stats):
             stats.str = stats.str + 1
             stats.con = stats.con + 1
             stats.health = stats.health + 1
-
     if stats.portrait in WaterTools_Chia_Farmers_List:
         stats.con = stats.con + 1
     if stats.portrait in SickleTools_Chia_Farmers_List:
