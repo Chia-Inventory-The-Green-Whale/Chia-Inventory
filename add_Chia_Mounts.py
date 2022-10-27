959d44df9e69fb0a7b544de0792de51bd939fba5
The Green Whale <110241587+Chia-Inventory-The-Green-Whale@users.noreply.github.com>
2022-10-27 09:05:05 +0700

    Update add_Chia_Mounts.py

--- add_Chia_Mounts.py
+++ add_Chia_Mounts.py
@@ -24,8 +24,6 @@ def modify_item_attributes():
                 item_list[i]["in-game-attributes"].pop("species_effect_5")
             if item_list[i]["in-game-attributes"].get("species_effect_6") != None:
                 item_list[i]["in-game-attributes"].pop("species_effect_6")
-            if item_list[i]["in-game-attributes"].get("species_effect_4") != None:
-                item_list[i]["in-game-attributes"].pop("species_effect_4")
 
             item_list[i]["item_type"] = "mount"
 
@@ -49,12 +47,12 @@ def modify_item_attributes():
                 item_list[i]["in-game-attributes"]["species_effect_1"]["value"] = 3
                 item_list[i]["in-game-attributes"]["species_effect_2"] = {}
                 item_list[i]["in-game-attributes"]["species_effect_2"]["type"] = "increase_bash"
-                item_list[i]["in-game-attributes"]["species_effect_2"]["factor"] = "random"
+                item_list[i]["in-game-attributes"]["species_effect_2"]["factor"] = "constant"
                 item_list[i]["in-game-attributes"]["species_effect_2"]["value"] = 1
                 item_list[i]["in-game-attributes"]["species_effect_3"] = {}
                 item_list[i]["in-game-attributes"]["species_effect_3"]["type"] = "increase_str"
                 item_list[i]["in-game-attributes"]["species_effect_3"]["factor"] = "random"
-                item_list[i]["in-game-attributes"]["species_effect_3"]["value"] = 5
+                item_list[i]["in-game-attributes"]["species_effect_3"]["value"] = 6
 
 
             if species == "warsheep":
@@ -65,7 +63,7 @@ def modify_item_attributes():
                 item_list[i]["in-game-attributes"]["species_effect_2"] = {}
                 item_list[i]["in-game-attributes"]["species_effect_2"]["type"] = "increase_luc"
                 item_list[i]["in-game-attributes"]["species_effect_2"]["factor"] = "random"
-                item_list[i]["in-game-attributes"]["species_effect_2"]["value"] = 5
+                item_list[i]["in-game-attributes"]["species_effect_2"]["value"] = 6
                 item_list[i]["in-game-attributes"]["species_effect_3"] = {}
                 item_list[i]["in-game-attributes"]["species_effect_3"]["type"] = "increase_dex"
                 item_list[i]["in-game-attributes"]["species_effect_3"]["factor"] = "random"
@@ -80,12 +78,11 @@ def modify_item_attributes():
                 item_list[i]["in-game-attributes"]["species_effect_2"] = {}
                 item_list[i]["in-game-attributes"]["species_effect_2"]["type"] = "increase_slash"
                 item_list[i]["in-game-attributes"]["species_effect_2"]["factor"] = "random"
-                item_list[i]["in-game-attributes"]["species_effect_2"]["value"] = 1
+                item_list[i]["in-game-attributes"]["species_effect_2"]["value"] = 2
                 item_list[i]["in-game-attributes"]["species_effect_3"] = {}
                 item_list[i]["in-game-attributes"]["species_effect_3"]["type"] = "increase_dex"
                 item_list[i]["in-game-attributes"]["species_effect_3"]["factor"] = "random"
                 item_list[i]["in-game-attributes"]["species_effect_3"]["value"] = 6
-
 
             if species == "magic marmot":
                 item_list[i]["in-game-attributes"]["species_effect_1"] = {}
@@ -95,22 +92,15 @@ def modify_item_attributes():
                 item_list[i]["in-game-attributes"]["species_effect_2"] = {}
                 item_list[i]["in-game-attributes"]["species_effect_2"]["type"] = "increase_pierce"
                 item_list[i]["in-game-attributes"]["species_effect_2"]["factor"] = "random"
-                item_list[i]["in-game-attributes"]["species_effect_2"]["value"] = 1
+                item_list[i]["in-game-attributes"]["species_effect_2"]["value"] = 2
                 item_list[i]["in-game-attributes"]["species_effect_3"] = {}
                 item_list[i]["in-game-attributes"]["species_effect_3"]["type"] = "increase_int"
                 item_list[i]["in-game-attributes"]["species_effect_3"]["factor"] = "random"
-                item_list[i]["in-game-attributes"]["species_effect_3"]["value"] = 5
-
-
-            attr_list = ["increase_str", "increase_dex", "increase_con", "increase_int", "increase_wis", "increase_cha",
-                         "increase_luc"]
-            item_list[i]["in-game-attributes"]["random_attribute"] = {}
-            item_list[i]["in-game-attributes"]["random_attribute"]["type"] = choice(attr_list)
-            item_list[i]["in-game-attributes"]["random_attribute"]["factor"] = "constant"
-            item_list[i]["in-game-attributes"]["random_attribute"]["value"] = randint(10, 11) / 10
-            item_list[i]["in-game-attributes"]["mount_attribute"] = {}
-            item_list[i]["in-game-attributes"]["mount_attribute"]["type"] = "mount_stamina"
-            item_list[i]["in-game-attributes"]["mount_attribute"]["value"] = randint(100, 120)
+                item_list[i]["in-game-attributes"]["species_effect_3"]["value"] = 3
+                item_list[i]["in-game-attributes"]["species_effect_4"] = {}
+                item_list[i]["in-game-attributes"]["species_effect_4"]["type"] = "increase_wis"
+                item_list[i]["in-game-attributes"]["species_effect_4"]["factor"] = "random"
+                item_list[i]["in-game-attributes"]["species_effect_4"]["value"] = 3
 
 
     with open('./library/item_list.json', 'w') as file:
